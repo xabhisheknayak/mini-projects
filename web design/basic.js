1856fa389b397c3e753e53ac7d2bcb0e1dcfd9fa
xabhisheknayak <egaming748@gmail.com>
2026-08-04 04:04:12 +0530

    file making

---
+++ web design/basic.js
@@ -0,0 +1,5 @@
+console.log("hello this is my first web page")
+// window.alert("hello this is my first web page")
+let a =10;
+console.log("the value of a is " + a)
+console.log(`the value of a is ${a}`)
