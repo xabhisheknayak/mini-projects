047ea7aa7b050f0fd186bafb52adcad5ff281bf9
xabhisheknayak <egaming748@gmail.com>
2026-08-19 15:05:34 +0530

    class question

--- dsa-class.c/class1.c
+++ dsa-class.c/class1.c
@@ -1,6 +1,15 @@
+//write a program to find factorial of a number using recursion
 #include <stdio.h>
-
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
 int main() {
+    
+    int n=10;
+    printf("Factorial of %d is %d\n", n, factorial(n));
     
     return 0;
 }
