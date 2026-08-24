8f0a5185cdc19c71df5e6d92eb1785e9ef737c7d
xabhisheknayak <egaming748@gmail.com>
2026-08-25 04:12:31 +0530

    js lec

--- dsa-class.c/class1.c
+++ dsa-class.c/class1.c
@@ -1,15 +1,18 @@
-//write a program to find factorial of a number using recursion
+// write a program to find factorial of a number using recursion
 #include <stdio.h>
-int factorial(int n) {
-    if (n == 0) {
+int factorial(int n)
+{
+    if (n == 0)
+    {
         return 1;
     }
     return n * factorial(n - 1);
 }
-int main() {
-    
-    int n=10;
+int main()
+{
+
+    int n = 10;
     printf("Factorial of %d is %d\n", n, factorial(n));
-    
+
     return 0;
 }
--- dsa-class.c/class1q2.c
+++ dsa-class.c/class1q2.c
@@ -1,18 +1,21 @@
-//fibonacci series using recursion
+// fibonacci series using recursion
 #include <stdio.h>
-int fibonacci(int n) {
-    if (n <= 1) {
+int fibonacci(int n)
+{
+    if (n <= 1)
+    {
         return n;
     }
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-int main() {
-    int n=10;
-    for (int
- i=0; i < n; i++) {
+int main()
+{
+    int n = 10;
+    for (int i = 1; i <= n; i++)
+    {
         printf("%d ", fibonacci(i));
     }
-    
+
     return 0;
 }
